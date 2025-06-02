eb079e52216de891b460779890917f4a09995c98
ling530aup <meilingxie520@qq.com>
2025-06-02 18:00:53 +0800

    add model

--- src/main/java/com/example/igniteserver/IgniteServerApplication.java
+++ src/main/java/com/example/igniteserver/IgniteServerApplication.java
@@ -1,25 +1,14 @@
 package com.example.igniteserver;
 
-import org.apache.ignite.Ignite;
-import org.apache.ignite.Ignition;
-import org.apache.ignite.configuration.IgniteConfiguration;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class IgniteServerApplication {
 
-    @Autowired
-    private IgniteConfiguration igniteConfiguration;
 
     public static void main(String[] args) {
         SpringApplication.run(IgniteServerApplication.class, args);
     }
 
-    @Bean
-    public Ignite igniteInstance() {
-        return Ignition.start(igniteConfiguration);
-    }
-} 
+}
